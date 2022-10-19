419d2a1943f3fd670325e8c46ed2ddefc9d48178
sara sallat <sallatsara@gmail.com>
2022-10-19 15:49:37 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+ function divide(num1,num2){
+    return num1/num2;
+ }
+  const square=(num)=>{
+    let result = 1;
+    for(var i = 0; i < num; i++) {
+        result *= num;
+    }
+    return result;
+
+  }
+  const add=(number1,number2)=>{ return number1+number2;}
+
